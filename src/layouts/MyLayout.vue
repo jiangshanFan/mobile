f04c47a3ac50c9c9a468832f77f47152a19d8e55
jiangshan.fan <745989449@qq.com>
2019-06-27 09:11:55 +0800

    git init

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round aria-label="Back">
+          <q-icon name="fas fa-angle-left" />
+        </q-btn>
+
+        <q-toolbar-title>
+          yfpo
+        </q-toolbar-title>
+
+        <!--<div>Quasar v{{ $q.version }}</div>-->
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+/* eslint-disable */
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  },
+  created() {
+
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page>
+    <!-- place QPageSticky at end of page -->
+    <q-page-sticky expand position="top">
+      <q-input outlined v-model="search" label="Label" placeholder="请输入项目名称或模号" dense>
+        <template v-slot:append>
+          <q-icon v-if="search !== ''" name="close" @click="search = ''" class="cursor-pointer" />
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-page-sticky>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+/* eslint-disable */
+import { login, } from '../axios/api'
+export default {
+  name: 'PageIndex',
+  async created() {
+    let res = await login({accountName: 'admin', pwd: 'Ybsx#123', type: 1,});
+    if (res.status === 1) {
+      console.log('login');
+    }
+
+  },
+
+  data() {
+    return {
+      search: '',
+    }
+  },
+}
+</script>
