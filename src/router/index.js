27ba8afdc5844d05ac7457e726c115d4e1e5377a
jiangshan.fan <745989449@qq.com>
2019-07-02 17:33:44 +0800

    add Router.beforeEach in /router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
@@ -20,7 +21,23 @@ export default function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
-  })
+  });
+
+  Router.beforeEach((to, from, next) => {
+    if (!sessionStorage.getItem('token')) {
+      if (to.matched.length > 0 && !to.matched.some(record => record.meta.requiresAuth)) {
+        next()
+      } else {
+        next({ path: '/login' })
+      }
+    } else {
+      if (to.path !== '/login') {
+        next()
+      } else {
+        next(from.fullPath)
+      }
+    }
+  });
 
   return Router
 }
